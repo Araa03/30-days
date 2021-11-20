4691fae772972acac6935a81c1329ad2fcf1db5f
School Use <School@aras-mbp.mynetworksettings.com>
2021-11-20 14:12:27 -0500

    initial commit

---
+++ Progamee/src/Programee.java
@@ -0,0 +1,17 @@
+
+public class Programee {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		while(i < 10) {
+			System.out.println("Your Variable = " + i);
+			i = i + 1;
+		}
+		
+		
+		
+	}
+
+}
