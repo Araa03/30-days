ea6faaad625c935662cfe6ef4fcb1d989d61ad0a
School Use <School@aras-mbp.mynetworksettings.com>
2021-11-23 17:49:24 -0500

    add input from file.

--- Progamee/src/Programee.java
+++ Progamee/src/Programee.java
@@ -1,17 +1,27 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 public class Programee {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner in = new Scanner(new File("student.txt"));
 		
-		int i = 0;
+		List<String> students = new ArrayList<String>();
 		
-		while(i < 10) {
-			System.out.println("Your Variable = " + i);
-			i = i + 1;
+		while(in.hasNextLine()) {
+			students.add(in.nextLine());
+		}
+		
+		for(int i = 0; i < students.size();i = i + 1) {
+			System.out.println("name: " + students.get(i));
+		}
+		in.close();
+	
+		
+		
 		}
 		
 		
 		
 	}
 
-}
